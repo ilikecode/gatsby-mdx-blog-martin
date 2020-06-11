47c6d17d5dc80a181649f2976cf4d352121baa71
ilikecode <membershipslogin@gmail.com>
2020-06-10 17:57:56 -0700

    Add Post directory with 4 sample posts. Add Themes directory and theme file.

---
+++ src/themes/theme.js
@@ -0,0 +1,32 @@
+export default {
+	fonts: {
+		main: "Raleway, sans-serif",
+	},
+	colors: {
+		white: "hsl(0,0%, 100%)",
+		main1: "hsl(207,70%, 59%)",
+		main2: "hsl(207,70%, 94%)",
+		dark1: "hsl(227,2%, 12%)",
+		dark2: "hsl(227,2%, 26%)",
+		dark3: "hsl(227,2%, 64%)",
+		light1: "hsl(0,0%, 97%)",
+		light2: "hsl(0,0%, 99%)",
+	},
+	breakpoints: {
+		xSmall: "only screen and (max-width: 40rem)",
+		small: "only screen and (max-width: 50rem)",
+		medium: "only screen and (max-width: 65rem)",
+		large: "only screen and (max-width: 75rem)",
+		xlarge: "only screen and (min-width: 75rem)",
+	},
+	spacings: {
+		xxSmall: ".25rem",
+		xSmall: ".5rem",
+		small: "1rem",
+		medium: "2rem",
+		large: "3rem",
+	},
+	shadows: {
+		shadow1: "0 2px 10px rgba(0,0,0,.1), 3px 5px 20px rgba(0,0,0,.1)",
+	}
+}
